b3500fb1fe6f30398e9f4af53e9ecf0c5b3a15b6
Vicheka Phat <vickphat@gmail.com>
2021-05-17 23:00:04 -0400

    added functions to view all employees by role and departments, added function to update

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const connection = mysql.createConnection({
   user: 'root',
   password: 'root',
   database: 'employees_DB',
+});
 
+// Connect to the mysql server and sql database
+connection.connect((err) => {
+  if (err) throw err;
+  // Run the startMenu function after the connection is made to prompt the user with choices
+  startMenu();
 });
 
 // Command-line prompts
@@ -22,8 +28,8 @@ const startMenu = () => {
     message: "How would you like to proceed?",
     choices: [
       "View All Employees",
-      "View All Employee's By Roles",
-      "View all Emplyees By Deparments",
+      "View All Employees By Roles",
+      "View all Employees By Departments",
       "Update Employee",
       "Add Employee",
       "Add Role",
@@ -37,10 +43,10 @@ const startMenu = () => {
         case "View All Employees":
           viewAllEmployees();
           break;
-        case "View All Employee's By Roles":
+        case "View All Employees By Roles":
           viewRoles();
           break;
-        case "View all Emplyees By Deparments":
+        case "View all Employees By Departments":
           viewDepartments();
           break;
         case "Update Employee":
@@ -64,25 +70,81 @@ const startMenu = () => {
     });
 };
 
+// Function to view all employees 
 const viewAllEmployees = () => {
 
-  let data = "SELECT * FROM employee";
+  let data = "SELECT * FROM employee LEFT JOIN role ON role_id = role.id;";
   connection.query(data, (err, res) => {
+
     if (err) throw err;
     console.table("All Employees:", res);
     startMenu();
   });
 };
 
+// Function to view all employees by roles
+const viewRoles = () => {
+
+  let data = "SELECT * FROM role LEFT JOIN employee ON role.id = role_id;";
+  connection.query(data, (err, res) => {
+
+    if (err) throw err;
+    console.table("All Roles:", res);
+    startMenu();
+  });
+};
+
+// Function to view all employees by departments
+const viewDepartments = () => {
+
+  let data = "SELECT * FROM department LEFT JOIN role ON department_id = department.id LEFT JOIN employee ON role_id = role.id;";
+  connection.query(data, (err, res) => {
+
+    if (err) throw err;
+    console.table("All Departments:", res);
+    startMenu();
+  });
+};
+
+// Function to update employee role
+const updateEmployee = () => {
+  inquirer.prompt([
+    {
+      name: "id",
+      type: "input",
+      message: "Please enter employee's ID number",
+    }
+  ])
+  .then((answer) => {
+
+    const ID = answer.id
+    inquirer.prompt([
+      {
+        name: "roleId",
+        type: "input",
+        message: "Please enter the role id",
+      }
+    ])
+    .then((answer) => {
+
+      const employeeRoleID = answer.roleId
+      let data = "UPDATE employee SET role_id=? WHERE id=?"
+
+      connection.query(data, [employeeRoleID, ID], (err,res) => {
+        if(err) throw err;
+        console.table(res);
+        startMenu();
+      });
+    });
+  });
+}
 
 
 
 
 
 
-// Connect to the mysql server and sql database
-connection.connect((err) => {
-  if (err) throw err;
-  // Run the startMenu function after the connection is made to prompt the user with choices
-  startMenu();
-});
+
+
+
+
